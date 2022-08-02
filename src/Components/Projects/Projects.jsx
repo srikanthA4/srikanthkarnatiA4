5e399a7b5652cbf79ec6b341d4b6327ac9ee87e7
srikanth <94035252+srikanthA4@users.noreply.github.com>
2022-08-02 12:38:41 +0530

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -15,7 +15,7 @@ export const Projects = () => {
         </h1>
         <div align="center">
           <h1> Demo Credentials</h1>
-          <h2 > User:ksreddy12@gmail.com |paswword:123456</h2>
+          <h2 > User:ksreddy123@gmail.com |paswword:123456</h2>
           <h2> Emart-Admin:admin12@gmail.com|password:123456</h2>
           
 
